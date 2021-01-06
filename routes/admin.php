e34698b6ff4d5ca665e06a11a9f2c397264bd017
ashokkumar2342 <ashokkumar2342@gmail.com>
2021-01-06 18:00:52 +0530

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ Route::get('refreshcaptcha', 'Auth\LoginController@refreshCaptcha')->name('admin
 Route::get('logout', 'Auth\LoginController@logout')->name('admin.logout.get');
 Route::post('login', 'Auth\LoginController@login');
 Route::get('register', 'Auth\LoginController@register')->name('admin.register');
-Route::post('register-store', 'Auth\LoginController@registerStore')->name('admin.register.store');
+// Route::post('register-store', 'Auth\LoginController@registerStore')->name('admin.register.store');
 Route::get('otp-verify', 'Auth\LoginController@otpVerify')->name('admin.otp-verify');
 Route::group(['middleware' => 'admin'], function() {
 	Route::get('dashboard', 'DashboardController@index')->name('admin.dashboard');
