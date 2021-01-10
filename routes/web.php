5055bebe4226eb738c6cbc1ae0a5cc23f7ba9d9e
ashokkumar2342 <ashokkumar2342@gmail.com>
2021-01-10 12:32:12 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -86,6 +86,8 @@ Route::get('image', function() {
   
   // exec("java -jar pdfbox-app.jar ExtractImages ak.pdf");
   exec("java -jar pdfbox-app.jar ExtractText ak.pdf");
+  // exec("java -jar pdfbox-app.jar PDFToImage -imageType png -dpi 300 -cropbox 0 0 290 260 ak.pdf");
+  // exec("java -jar pdfbox-app.jar PDFToImage -imageType png -dpi 300 -cropbox 290 260 0 0 secure3.pdf");
 
    // create an image manager instance with favored driver
    $manager = new Intervention\Image\ImageManager();
